589a6acaac8e5a0f59cc1754c38a8176ba22fdf3
DESKTOP-BR9481H\Paul <babucke.paul@mail.com>
2022-08-13 01:17:06 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAnalytics(() => getAnalytics()),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
   ],
-  providers: [],
+  providers: [
+    ScreenTrackingService,UserTrackingService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'recipetool-1ac74',
+    appId: '1:361241405527:web:ff20d78f9d4aacd8711b12',
+    storageBucket: 'recipetool-1ac74.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyDuA5dKQkXNT5kUQW_gepABarz6c4wgt9U',
+    authDomain: 'recipetool-1ac74.firebaseapp.com',
+    messagingSenderId: '361241405527',
+    measurementId: 'G-LRCQ6RLFPL',
+  },
   production: true
 };
